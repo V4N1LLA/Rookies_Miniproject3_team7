7899775d46057ef1fbb3b3c0c265975fce09cbcb
lemongza <92498054+lemongza@users.noreply.github.com>
2025-07-09 10:26:42 +0900

    라우터 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signups";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<MainPage />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/diary" element={<Calendar />} />
+        <Route path="/diary/DiaryWrite" element={<DiaryWrite />} />
+        <Route path="/diary/DiaryDetail/:id" element={<DiaryDetail />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
