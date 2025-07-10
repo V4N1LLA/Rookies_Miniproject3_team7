5e6f2ecc26f371b36cdff98f7421b49c876b2fc6
lemongza <92498054+lemongza@users.noreply.github.com>
2025-07-10 11:02:56 +0900

    회원가입 username->name

--- frontend/src/pages/auth/Signup.js
+++ frontend/src/pages/auth/Signup.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
@@ -17,7 +17,7 @@ export default function Signup() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, username, password }),
+        body: JSON.stringify({ email, name, password }),
       });
 
       if (res.ok) {
@@ -53,8 +53,8 @@ export default function Signup() {
               type="text"
               placeholder="이름"
               className="w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-400 shadow-sm"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               required
             />
           </div>
